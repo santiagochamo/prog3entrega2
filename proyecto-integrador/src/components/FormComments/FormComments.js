5c71976c07b4380beb2940dc7a9f6ab8f30dccf9
30Thiago <grimthiago@gmail.com>
2023-06-12 00:07:47 -0300

    form coments

--- proyecto-integrador/src/components/FormComments/FormComments.js
+++ proyecto-integrador/src/components/FormComments/FormComments.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react'
+import { Text, View, TouchableOpacity, FlatList } from 'react-native'
 
 export default class FormComments extends Component {
+    constructor(props){
+    super(props)
+    }
     render() {
         return (
-            <div>
-                
-            </div>
+            <View>
+                <TouchableOpacity onPress={() => this.props.navigation.navigate('Comments')}>
+                    <Text>Dejá tu comentario:</Text>
+                </TouchableOpacity>
+            </View>
         )
     }
 }
