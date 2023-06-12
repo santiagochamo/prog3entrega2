6b4b3286f889e53755f1642bf34b88ec4b969a80
FelixDuhaldeUdesa <fduhalde@udesa.edu.ar>
2023-06-12 11:56:30 -0300

    screen Comments desarrollado

--- proyecto-integrador/src/screens/Comments/Comments.js
+++ proyecto-integrador/src/screens/Comments/Comments.js
@@ -2,20 +2,34 @@
 import React, { Component } from 'react'
 import { auth, db } from '../../firebase/config'
 import { Text, View, TouchableOpacity, FlatList } from 'react-native'
+import FormComments from '../../components/FormComments/FormComments'
 
 class Comments extends Component {
 
     constructor(props){
         super(props)
         this.state = {
-
+            data:{}
         }
 
     }
+
+    componentDidMount(){
+        db.collection('posts').doc(this.props.route.params.id).onSnapshot(doc => {
+            this.setState({data:doc.data()}, ()=> console.log(this.state.data))
+        })
+    }
+
     render() {
         return (
             <View>
-                <Text>Comments</Text>
+                <Text>Comentarios</Text>
+                <Flatlist
+                data={this.state.data.comentarios}
+                keyExtractor={item => item.createdAt.toString()}
+                renderItem={({item}) => <Text>{item.comentario}</Text>}
+                />
+                <FormComments id={this.props.route.params.id}/>
             </View>
         )
     }
